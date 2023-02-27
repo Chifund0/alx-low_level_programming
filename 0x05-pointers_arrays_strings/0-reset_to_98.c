dabd9b673f742cc763d2797aaf8177d1c987eb27
Chifund0 <chifundonyakwawa@gmail.com>
2023-02-27 12:10:24 -0800

    this is hard

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ * main - fn tkes pointer to int as parameta & updates de value it points to 98
+ *
+ * Return: Always 0;
+ *
+ */
+
+
+int _putchar(char n);
+
+int main(void)
+
+{
+	char n = '98';
+
+	_putchar(n);
+	return (0);
+}
