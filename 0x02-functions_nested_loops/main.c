6213c4554541b246734567e46a7809a013101995
Chifund0 <chifundonyakwawa@gmail.com>
2023-03-05 16:37:47 -0800

    hard things indeed

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
+void print_alphabet(void);
+
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
- * Return: Always 0.
+ * Return: always 0
+ *
  */
+
 int main(void)
 {
-	print_alphabet();
-	return (0);
-}
+    print_alphabet();
+    return (0);
+} 
