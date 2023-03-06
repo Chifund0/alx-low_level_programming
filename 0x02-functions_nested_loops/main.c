8d32fc88e311f36feeaed1c5899fdbe56eb16c28
Chifund0 <chifundonyakwawa@gmail.com>
2023-03-05 17:05:19 -0800

    mmmh

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -3,7 +3,7 @@
 void print_alphabet(void);
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students
  *
  * Return: always 0
  *
@@ -13,4 +13,4 @@ int main(void)
 {
     print_alphabet();
     return (0);
-} 
+}  
