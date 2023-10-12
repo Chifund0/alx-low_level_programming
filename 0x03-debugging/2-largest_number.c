3721b8f377024e7e78d1c9a517f808285ba087df
Chifund0 <chifundonyakwawa@gmail.com>
2023-10-12 02:02:42 -0700

    CMD prints the largest of three integers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -28,9 +28,17 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else
+	else if (a >=b && a >= c)
 	{
 		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
 	}
 
 	return (largest);
