b1b75da4e901793a190a5b6b26a2d8f768ccb473
Chifund0 <chifundonyakwawa@gmail.com>
2023-10-13 01:37:04 -0700

    CMD converts a date to the day of year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ void print_remaining_days(int month, int day, int year)
 
 	leap_year = 0;
 
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 	{
 		leap_year = 1;
 	}
