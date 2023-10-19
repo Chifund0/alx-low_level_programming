9d5732c172f0c3ef0df79d002b01c4fb0cab1a4e
Chifund0 <chifundonyakwawa@gmail.com>
2023-10-19 03:48:21 -0700

    CMD takes a pointer & updates its value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - derefencing pointers, example with int and char types
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
